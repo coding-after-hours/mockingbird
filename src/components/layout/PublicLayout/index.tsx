39c9acf4ab7f2600e490cd01036c676d18df0f65
Jacob Asper <jacobasper191@gmail.com>
2023-08-29 15:51:34 -0400

    remove all pages besides about and add sign-up route

--- src/components/layout/PublicLayout/index.tsx
+++ src/components/layout/PublicLayout/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from "~/components/lib/router/Link";
 import { Outlet } from "~/components/lib/router/Outlet";
 import { Suspense } from "~/components/provider/Suspense";
-import { errorButton, successButton } from "~/constants/buttonColor";
+import { successButton } from "~/constants/buttonColor";
 
 export const PublicLayout = () => {
 	return (
-		<main className="h-full min-h-screen bg-slate-900 p-4">
+		<main className="p-4 h-full min-h-screen bg-slate-900">
 			<div className="flex flex-wrap gap-4 pb-4">
 				<Link
 					to="/"
@@ -14,34 +14,10 @@ export const PublicLayout = () => {
 					to /
 				</Link>
 				<Link
-					to="/nest-one"
+					to="/sign-up"
 					className={successButton}
 				>
-					to /nest-one
-				</Link>
-				<Link
-					to="/about"
-					className={successButton}
-				>
-					to /about
-				</Link>
-				<Link
-					to="/posts"
-					className={successButton}
-				>
-					to /posts
-				</Link>
-				<Link
-					to="/nest-posts"
-					className={successButton}
-				>
-					to /nest-posts
-				</Link>
-				<Link
-					to="/not-found"
-					className={errorButton}
-				>
-					to /not-found
+					to /sign-up
 				</Link>
 			</div>
 
